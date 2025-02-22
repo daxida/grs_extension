4269408b8aa41042daf67c49f41de7141d999c0e
daxida <galmaw@hotmail.com>
2025-02-22 12:33:00 +0100

    Improve popup colors

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const colorPicker = document.getElementById('color-picker');
-  const saveButton = document.getElementById('save-button');
+  let debounceTimer;
 
   // Load the stored color if it exists
   chrome.storage.local.get('selectedColor', function(result) {
@@ -9,18 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  // Save the color to chrome.storage.local when the user clicks the Save button
-  saveButton.addEventListener('click', function() {
+  // Debounced function to save color once user stops input
+  function saveColor() {
     const selectedColor = colorPicker.value;
     chrome.storage.local.set({ selectedColor: selectedColor });
-    // Test
-    chrome.storage.local.get("selectedColor", (data) => {
-      console.log("Stored color:", data.selectedColor);
-      alert("Stored color: " + data.selectedColor);
-    });
 
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length === 0) return;
+
       chrome.tabs.sendMessage(tabs[0].id, { action: "runScan" }, (response) => {
         if (chrome.runtime.lastError) {
           console.error("Error sending message:", JSON.stringify(chrome.runtime.lastError));
@@ -29,5 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       });
     });
+  }
+
+  colorPicker.addEventListener('input', function() {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(saveColor, 500);
   });
 });
